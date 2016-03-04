0c2344fa7f91c03f4ab1b603acefa2c8cd916e94
Debora Cristina <fcristinadebora@gmail.com>
2016-03-03 21:25:41 -0300

    Added files via upload

--- sistemas-digitais/t1.v
+++ sistemas-digitais/t1.v
@@ -1,14 +1,40 @@
-module clock;
 
-	reg clk;
+module led_control(
+	input i_gnd,
+	input i_clock,
+	output o_gnd,
+	output o_clock
+	);
 
-	always #1 clk = ~clk;
+  assign o_gnd = i_gnd;
+  assign o_clock = i_clock;
 
-	initial begin
-		$dumpvars(0,clk);
-			clk <= 0;
-			#200;
-		$finish;
-	end
+endmodule
 
+module led;
+
+	led_control ld(
+		i_gnd,
+		i_clock,
+		o_gnd,
+		o_clock
+	);
+
+	reg i_gnd, i_clock;
+	
+	wire 
+		o_gnd,
+		o_clock
+	;
+
+    always #1 i_clock = ~i_clock;
+
+  
+  initial begin
+    $dumpvars(0, ld);
+    i_gnd <= 1'b0;
+    i_clock <=  1'b0;
+    #500;
+    $finish;
+  end
 endmodule
