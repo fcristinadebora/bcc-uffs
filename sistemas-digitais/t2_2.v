4ad0deade1d3e9c05075a9d873037a17c9f45ab5
Debora Cristina <fcristinadebora@gmail.com>
2016-03-10 16:07:57 -0300

    Update t2_2.v

--- sistemas-digitais/t2_2.v
+++ sistemas-digitais/t2_2.v
@@ -1,55 +1,35 @@
-module counter(
-	input  ,
-	output q
-);
+module led_test(
+	//Entradas/Saídas do modulo
+	input CLOCK_50,
+	output [1:0] LEDG
+	);
+	//Fim entradas/saidas
 
-	reg [26:0]count, state;
-	wire q;
+	//Registradores armazenam valores até que sejam alterados
+	//gnd = constante em 0
+	//state = liga/desliga o led
+	//counter = vetor até 2^26 para regular o tempo do led
+	//reset = inicio do clock
+	reg gnd = 1'b0;
+	reg state = 1'b0;
+	reg [26:0]count = 26'd0;
 
-	assign q = state;
-
-	always@(posedge clock) begin
-		if(count == 26'd50000000000)
+	//"amarrando" os elementos
+	assign LEDG[0] = gnd;
+	assign LEDG[1] = state;
+	
+	//durante a execução
+	always @ (posedge CLOCK_50) begin
+		if(count == 26'd50000000)
 			begin	
-				count <= 26'd0;
-				//reseta o count
-				if(state == 1)
-					state <= 1'b0;//muda estado							
-				else		
-					state <= 1'b1;//muda estado
-				
+				count <= 26'd0; //reseta o count
+				state <= ~state; //muda o estado
 			end
 		else
 			begin
 				//incrementa count
 				count <= count + 1;
 			end
-	end
-
-endmodule
-
-// input CLOCK_50
-// input [0:0] KEY
-// output [0:0] LEDG
-
-
-
-module led_test;
-
-	counter cnt();
-	
-	initial begin
-		$dumpvars(0, clock);
-		clock
-
-		$finish;
-	end  	
-  $dumpvars(0, ld);
-    #10;
-    i_gnd <= 1'b0;
-    i_clock <=  1'b0;
-    #500;
-    $finish;
-  
+	end  
 
 endmodule
